110c1a751a3a4f339bfb3c7fa468174f7d1da587
105928301 <105928301@student.swin.edu.au>
2025-05-12 09:58:09 +1000

    fixed indentation

--- settings.php
+++ settings.php
@@ -5,6 +5,6 @@ $pwd = "";
 $sql_db = "clickmaxxing_db";
 $conn = mysqli_connect($host, $user, $pwd, $sql_db);
 if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
