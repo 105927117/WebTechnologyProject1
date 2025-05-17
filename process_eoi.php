027cf9d47b4881a54b1995e926dc981eba804950
Kunvuth You <105730302@student.swin.edu.au>
2025-05-17 16:52:12 +1000

    fixed typo

--- process_eoi.php
+++ process_eoi.php
@@ -112,7 +112,7 @@ if ($_SERVER["REQUEST_METHOD"]  == "POST") {
             `Phone-number` VARCHAR(20) NOT NULL,
             `Skills` VARCHAR(50) NOT NULL,
             `Other-skills` TEXT DEFAULT NULL
-            'status' ENUM('New','Current','Final') NOT NULL DEFAULT 'Final'
+            'status' ENUM('New','Current','Final') NOT NULL DEFAULT 'New'
         )";
         if (mysqli_query($conn, $create_table)){
             $result = mysqli_query($conn, $query);
